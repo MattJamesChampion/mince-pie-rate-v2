6d8f084eca4d4c3bc4557a33ada875822fc05dfd
Matthew Champion <mattjameschampion@gmail.com>
2019-09-14 18:32:20 +0100

    Move the MincePie model into the Domain folder/namespace

--- MincePieRateV2/MincePieRateV2.DAL/Data/ApplicationDbContext.cs
+++ MincePieRateV2/MincePieRateV2.DAL/Data/ApplicationDbContext.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using MincePieRateV2.Web.Models;
+using MincePieRateV2.Web.Models.Domain;
 
 namespace MincePieRateV2.DAL.Data
 {
