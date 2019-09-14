088561f2ed04792e3f5c9891966ddc9393ef1891
Matthew Champion <mattjameschampion@gmail.com>
2019-09-14 18:41:42 +0100

    Add empty IRepository interface and base implementation

---
+++ MincePieRateV2/MincePieRateV2.DAL/Repositories/BaseRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MincePieRateV2.DAL.Repositories
+{
+    public class BaseRepository<T>: IRepository<T>
+    {
+    }
+}
