1261f0a3b9845bd6df1ad585509a1438ba9c2df6
Matthew Champion <mattjameschampion@gmail.com>
2019-09-15 11:54:14 +0100

    Add basic read functionality to IRepository and BaseRepository

--- MincePieRateV2/MincePieRateV2.DAL/Repositories/BaseRepository.cs
+++ MincePieRateV2/MincePieRateV2.DAL/Repositories/BaseRepository.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using MincePieRateV2.DAL.Data;
+using MincePieRateV2.Web.Models.Domain;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MincePieRateV2.DAL.Repositories
 {
-    public class BaseRepository<T>: IRepository<T>
+    public class BaseRepository<T> : IRepository<T> where T: class, new()
     {
+        public ApplicationDbContext Context;
+        public DbSet<T> Table { get; }
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            Context = context;
+            Table = context.Set<T>();
+        }
+
+        public T GetEntity(Func<T, bool> predicate)
+        {
+            return Table.Where(predicate).FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetEntities(Func<T, bool> predicate = null)
+        {
+            if (predicate != null)
+            {
+                return Table.Where(predicate);
+            }
+            else
+            {
+                return Table;
+            }
+        }
     }
 }
--- MincePieRateV2/MincePieRateV2.DAL/Repositories/IRepository.cs
+++ MincePieRateV2/MincePieRateV2.DAL/Repositories/IRepository.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MincePieRateV2.DAL.Repositories
 {
     public interface IRepository<T>
     {
+        T GetEntity(Func<T, bool> predicate);
+        IEnumerable<T> GetEntities(Func<T, bool> predicate=null);
     }
 }
