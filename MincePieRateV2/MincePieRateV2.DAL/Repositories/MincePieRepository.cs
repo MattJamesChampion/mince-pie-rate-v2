0b6074f166d743c8db2bf90cb3114973dd3fd2d4
Matthew Champion <mattjameschampion@gmail.com>
2019-09-15 12:14:03 +0100

    Add MincePieRepository

---
+++ MincePieRateV2/MincePieRateV2.DAL/Repositories/MincePieRepository.cs
@@ -0,0 +1,15 @@
+﻿using MincePieRateV2.DAL.Data;
+using MincePieRateV2.Web.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MincePieRateV2.DAL.Repositories
+{
+    public class MincePieRepository: BaseRepository<MincePie>
+    {
+        public MincePieRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
+        {
+        }
+    }
+}
