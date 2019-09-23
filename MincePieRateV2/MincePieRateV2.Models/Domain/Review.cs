c87214aa98cf26b6f55dd43fa7d4eaf19befe5a9
Matthew Champion <mattjameschampion@gmail.com>
2019-09-23 21:54:59 +0100

    Add Review model

---
+++ MincePieRateV2/MincePieRateV2.Models/Domain/Review.cs
@@ -0,0 +1,27 @@
+﻿using MincePieRateV2.Models.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MincePieRateV2.Models.Domain
+{
+    public class Review : ModelMetadata
+    {
+        //Primary Key
+        public int Id { get; set; }
+
+        //Foreign Keys
+        public MincePie MincePie { get; set; }
+
+        //References
+        public int MincePieId { get; set; }
+
+        //Properties
+        public int PastryRating { get; set; }
+        public int FillingRating { get; set; }
+        public int AppearanceRating { get; set; }
+        public int AromaRating { get; set; }
+        public int ValueForMoney { get; set; }
+        public string FreeTextReview { get; set; }
+    }
+}
