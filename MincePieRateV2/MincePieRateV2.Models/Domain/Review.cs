c8bcee3cdf723fd6ee605581ed253c7f45b7eb62
Matthew Champion <mattjameschampion@gmail.com>
2019-09-23 22:28:18 +0100

    Add DisplayName attributes to the Review model

--- MincePieRateV2/MincePieRateV2.Models/Domain/Review.cs
+++ MincePieRateV2/MincePieRateV2.Models/Domain/Review.cs
@@ -1,6 +1,7 @@
 ﻿using MincePieRateV2.Models.Metadata;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace MincePieRateV2.Models.Domain
@@ -14,14 +15,21 @@ namespace MincePieRateV2.Models.Domain
         public MincePie MincePie { get; set; }
 
         //References
+        [DisplayName("Mince Pie")]
         public int MincePieId { get; set; }
 
         //Properties
+        [DisplayName("Pastry Rating")]
         public int PastryRating { get; set; }
+        [DisplayName("Filling Rating")]
         public int FillingRating { get; set; }
+        [DisplayName("Appearance Rating")]
         public int AppearanceRating { get; set; }
+        [DisplayName("Aroma Rating")]
         public int AromaRating { get; set; }
+        [DisplayName("Value for Money")]
         public int ValueForMoney { get; set; }
+        [DisplayName("Free-text Review")]
         public string FreeTextReview { get; set; }
     }
 }
