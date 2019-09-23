614254bb17701b10e52c1e79a86e112e2b514f73
Matthew Champion <mattjameschampion@gmail.com>
2019-09-23 22:54:42 +0100

    Override GetEntity and GetEntities in ReviewRepository to include related data from the MincePie table

--- MincePieRateV2/MincePieRateV2.DAL/Repositories/ReviewRepository.cs
+++ MincePieRateV2/MincePieRateV2.DAL/Repositories/ReviewRepository.cs
@@ -1,7 +1,9 @@
-﻿using MincePieRateV2.DAL.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using MincePieRateV2.DAL.Data;
 using MincePieRateV2.Models.Domain;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MincePieRateV2.DAL.Repositories
@@ -10,6 +12,32 @@ namespace MincePieRateV2.DAL.Repositories
     {
         public ReviewRepository(ApplicationDbContext applicationDbContext) : base(applicationDbContext)
         {
+        }
+
+        private IQueryable<Review> GetTableWithIncludes()
+        {
+            return Table.Include(r => r.MincePie);
+        }
+
+        public override Review GetEntity(Func<Review, bool> predicate)
+        {
+            var tableWithIncludes = GetTableWithIncludes();
+
+            return tableWithIncludes.Where(predicate).FirstOrDefault();
+        }
+
+        public override IEnumerable<Review> GetEntities(Func<Review, bool> predicate = null)
+        {
+            var tableWithIncludes = GetTableWithIncludes();
+
+            if (predicate != null)
+            {
+                return tableWithIncludes.Where(predicate);
+            }
+            else
+            {
+                return tableWithIncludes;
+            }
         }
     }
 }
