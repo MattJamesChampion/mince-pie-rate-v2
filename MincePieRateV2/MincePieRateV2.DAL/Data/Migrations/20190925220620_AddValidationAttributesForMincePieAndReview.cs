03f83add832dcde33412c776d42b848a10743a1d
Matthew Champion <mattjameschampion@gmail.com>
2019-09-25 23:06:49 +0100

    Add missing migration for the validation attributes on MincePie and Review

---
+++ MincePieRateV2/MincePieRateV2.DAL/Data/Migrations/20190925220620_AddValidationAttributesForMincePieAndReview.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MincePieRateV2.Web.Data.Migrations
+{
+    public partial class AddValidationAttributesForMincePieAndReview : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FreeTextReview",
+                table: "Reviews",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "MincePies",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Brand",
+                table: "MincePies",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FreeTextReview",
+                table: "Reviews",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 500,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "MincePies",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Brand",
+                table: "MincePies",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50);
+        }
+    }
+}
