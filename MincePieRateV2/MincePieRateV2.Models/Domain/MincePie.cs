cb7692219c5b7ad7f9d48c205adc8c876af5f77f
Matthew Champion <mattjameschampion@gmail.com>
2019-09-25 22:37:54 +0100

    Add validation attributes to the MincePie and Review models

--- MincePieRateV2/MincePieRateV2.Models/Domain/MincePie.cs
+++ MincePieRateV2/MincePieRateV2.Models/Domain/MincePie.cs
@@ -1,6 +1,7 @@
 ﻿using MincePieRateV2.Models.Metadata;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,7 +11,11 @@ namespace MincePieRateV2.Models.Domain
     public class MincePie : ModelMetadata
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Brand { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
         public override string ToString()
         {
