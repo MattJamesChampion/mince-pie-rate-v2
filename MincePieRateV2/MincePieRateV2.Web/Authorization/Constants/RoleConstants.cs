b7cf5aaf42a2c7ac8f6615d1d2e4d728f0f71792
Matthew Champion <mattjameschampion@gmail.com>
2019-10-04 19:04:09 +0100

    Add basic roles and automatically set up an administrator in development environments

---
+++ MincePieRateV2/MincePieRateV2.Web/Authorization/Constants/RoleConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MincePieRateV2.Web.Authorization.Constants
+{
+    public static class RoleConstants
+    {
+        public const string UserRoleName = "User";
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
--- MincePieRateV2/MincePieRateV2.Web/Startup.cs
+++ MincePieRateV2/MincePieRateV2.Web/Startup.cs
@@ -17,6 +17,7 @@ using MincePieRateV2.DAL.Data;
 using MincePieRateV2.DAL.Repositories;
 using MincePieRateV2.Models.Domain;
 using MincePieRateV2.DAL.ActionFilters;
+using MincePieRateV2.Web.Authorization.Constants;
 
 namespace MincePieRateV2.Web
 {
@@ -43,6 +44,7 @@ namespace MincePieRateV2.Web
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
@@ -60,7 +62,7 @@ namespace MincePieRateV2.Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -90,6 +92,69 @@ namespace MincePieRateV2.Web
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+
+            CreateRoles(serviceProvider);
+            CreateDefaultUsers(serviceProvider, env);
+        }
+
+        private void CreateRoles(IServiceProvider serviceProvider)
+        {
+            //https://stackoverflow.com/q/42471866
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            var roleNames = new List<string>()
+            {
+                RoleConstants.UserRoleName,
+                RoleConstants.AdministratorRoleName,
+            };
+
+            foreach (var roleName in roleNames)
+            {
+                var hasRole = roleManager.RoleExistsAsync(roleName);
+                hasRole.Wait();
+
+                if (!hasRole.Result)
+                {
+                    var roleResult = roleManager.CreateAsync(new IdentityRole(roleName));
+                    roleResult.Wait();
+                }
+            }
+        }
+
+        private void CreateDefaultUsers(IServiceProvider serviceProvider, IWebHostEnvironment environment)
+        {
+            //https://stackoverflow.com/q/42471866
+            if (environment.IsDevelopment())
+            {
+                //This account is only for development and should not be used in production!
+                var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+                string email = "development_administrator@mincepierate.com";
+                string password = "DeleteThisAccountInProduction!1";
+                var testUser = userManager.FindByEmailAsync(email);
+                testUser.Wait();
+
+                if (testUser.Result == null)
+                {
+                    var administrator = new IdentityUser
+                    {
+                        Email = email,
+                        UserName = email
+                    };
+
+                    var newUser = userManager.CreateAsync(administrator, password);
+                    newUser.Wait();
+
+                    if (newUser.Result.Succeeded)
+                    {
+                        var newUserRole = userManager.AddToRoleAsync(administrator, "Administrator");
+                        newUserRole.Wait();
+                    }
+                }
+            }
+            else
+            {
+                return;
+            }
         }
     }
 }
