46b845b59c02c943bce0fb17001cd58e9cf0e3f7
Matthew Champion <mattjameschampion@gmail.com>
2019-10-10 18:29:40 +0100

    Create empty FileSystemImageManager implementation of IImageManager

---
+++ MincePieRateV2/MincePieRateV2.DAL/Managers/FileSystemImageManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace MincePieRateV2.DAL.Managers
+{
+    public class FileSystemImageManager : IImageManager
+    {
+        public async Task<Guid> AddImageAsync(IFormFile formFile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteImageAsync(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IFormFile> GetImageAsync(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
