a82ff7a0262a73935631a920c5644a5c49e2d08d
Matthew Champion <mattjameschampion@gmail.com>
2019-10-10 18:28:18 +0100

    Add the IImageManager interface

---
+++ MincePieRateV2/MincePieRateV2.DAL/Managers/IImageManager.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MincePieRateV2.DAL.Managers
+{
+    public interface IImageManager
+    {
+        Task<Guid> AddImageAsync(IFormFile formFile);
+        Task<IFormFile> GetImageAsync(Guid guid);
+        Task DeleteImageAsync(Guid guid);
+    }
+}
