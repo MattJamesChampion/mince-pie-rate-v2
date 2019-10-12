ff25c6ec0f4a1f7b942b7bfaf1d38c096c6d3ce5
Matthew Champion <mattjameschampion@gmail.com>
2019-10-12 11:56:28 +0100

    Add FileContentTypes constants

---
+++ MincePieRateV2/MincePieRateV2.CrossCutting/ImageHandling/Constants/FileContentTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MincePieRateV2.CrossCutting.ImageHandling.Constants
+{
+    public static class FileContentTypes
+    {
+        public const string jpg = "image/jpg";
+        public const string jpeg = "image/jpeg";
+        public const string pjpeg = "image/pjpeg";
+        public const string gif = "image/gif";
+        public const string xpng = "image/x-png";
+        public const string png = "image/png";       
+    }
+}
