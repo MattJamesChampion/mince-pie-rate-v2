865d3d5970bbe5d0c7902c7d687c6248242be74e
Matthew Champion <mattjameschampion@gmail.com>
2019-10-12 11:57:50 +0100

    Add FileExtensions constants

---
+++ MincePieRateV2/MincePieRateV2.CrossCutting/ImageHandling/Constants/FileExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MincePieRateV2.CrossCutting.ImageHandling.Constants
+{
+    public class FileExtensions
+    {
+        public const string jpg = ".jpg";
+        public const string jpeg = ".jpeg";
+        public const string png = ".png";
+        public const string gif = ".gif";
+    }
+}
