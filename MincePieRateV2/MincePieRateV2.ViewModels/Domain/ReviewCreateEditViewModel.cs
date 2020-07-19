a6bbc3a1fb2336858917bda69f97809e7d9da12f
Matthew Champion <mattjameschampion@gmail.com>
2020-07-19 09:49:29 +0100

    Consolidate shared viewmodel logic from ReviewCreateEditViewModel and ReviewDetailsViewModel into ReviewBaseViewModel

--- MincePieRateV2/MincePieRateV2.ViewModels/Domain/ReviewCreateEditViewModel.cs
+++ MincePieRateV2/MincePieRateV2.ViewModels/Domain/ReviewCreateEditViewModel.cs
@@ -6,45 +6,8 @@ using System.Text;
 
 namespace MincePieRateV2.ViewModels.Domain
 {
-    public class ReviewCreateEditViewModel
+    public class ReviewCreateEditViewModel : ReviewBaseViewModel
     {
-        //Primary Key
-        public int Id { get; set; }
-
-        //Foreign Keys
-        [DisplayName("Mince Pie")]
-        public MincePieDetailsViewModel MincePie { get; set; }
-
-        //References
-        [DisplayName("Mince Pie")]
-        [Required]
-        public int MincePieId { get; set; }
         public IEnumerable<MincePieDetailsViewModel> MincePies { get; set; }
-
-        //Properties
-        [DisplayName("Pastry Rating")]
-        [Required]
-        [Range(1, 10)]
-        public int PastryRating { get; set; }
-        [DisplayName("Filling Rating")]
-        [Required]
-        [Range(1, 10)]
-        public int FillingRating { get; set; }
-        [DisplayName("Appearance Rating")]
-        [Required]
-        [Range(1, 10)]
-        public int AppearanceRating { get; set; }
-        [DisplayName("Aroma Rating")]
-        [Required]
-        [Range(1, 10)]
-        public int AromaRating { get; set; }
-        [DisplayName("Value for Money")]
-        [Required]
-        [Range(1, 10)]
-        public int ValueForMoney { get; set; }
-        [DisplayName("Free-text Review")]
-        [DataType(DataType.MultilineText)]
-        [MaxLength(500)]
-        public string FreeTextReview { get; set; }
     }
 }
--- MincePieRateV2/MincePieRateV2.ViewModels/Domain/ReviewDetailsViewModel.cs
+++ MincePieRateV2/MincePieRateV2.ViewModels/Domain/ReviewDetailsViewModel.cs
@@ -6,44 +6,8 @@ using System.Text;
 
 namespace MincePieRateV2.ViewModels.Domain
 {
-    public class ReviewDetailsViewModel
+    public class ReviewDetailsViewModel : ReviewBaseViewModel
     {
-        //Primary Key
-        public int Id { get; set; }
-
-        //Foreign Keys
-        [DisplayName("Mince Pie")]
-        public MincePieDetailsViewModel MincePie { get; set; }
-
-        //References
-        [DisplayName("Mince Pie")]
-        [Required]
-        public int MincePieId { get; set; }
-
-        //Properties
-        [DisplayName("Pastry Rating")]
-        [Required]
-        [Range(1, 10)]
-        public int PastryRating { get; set; }
-        [DisplayName("Filling Rating")]
-        [Required]
-        [Range(1, 10)]
-        public int FillingRating { get; set; }
-        [DisplayName("Appearance Rating")]
-        [Required]
-        [Range(1, 10)]
-        public int AppearanceRating { get; set; }
-        [DisplayName("Aroma Rating")]
-        [Required]
-        [Range(1, 10)]
-        public int AromaRating { get; set; }
-        [DisplayName("Value for Money")]
-        [Required]
-        [Range(1, 10)]
-        public int ValueForMoney { get; set; }
-        [DisplayName("Free-text Review")]
-        [DataType(DataType.MultilineText)]
-        [MaxLength(500)]
-        public string FreeTextReview { get; set; }
+        
     }
 }
